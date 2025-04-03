40c4b735fc17eeafb81839236bce9923c9b047f0
Guocork <g1024536444@gmail.com>
2025-04-03 21:54:28 +0800

    wip fix issue

--- src/visitor.rs
+++ src/visitor.rs
@@ -1,14 +1,15 @@
-use crate::ast::{Expression, Span, Statement, Type};
+use crate::ast::{BinaryOp, Expression, LiteralValue, PrefixOp, Span, Statement, Type};
 
 pub trait StmtVisitor<T> {
     fn visit_block(&mut self, statements: &[Statement], span: &Span) -> T;
     fn visit_if(&mut self, condition: &Expression, then_branch: &Statement, else_branch: Option<&Statement>, span: &Span) -> T;
     fn visit_while(&mut self, condition: &Expression, body: &Statement, span: &Span) -> T;
     fn visit_for(&mut self, init: Option<&Statement>, condition: &Expression, increment: Option<&Statement>, body: &Statement, span: &Span) -> T;
-    fn visit_return(&mut self, value: Option<&Statement>, span: &Span) -> T;
+    fn visit_return(&mut self, value: Option<&Expression>, span: &Span) -> T;
     fn visit_expression(&mut self, expression: &Expression, span: &Span) -> T;
-    fn visit_variable_declaration(&mut self, name: &str, type_ann: Option<Type>, initializer: Option<&Statement>, span: &Span) -> T;
+    fn visit_variable_declaration(&mut self, name: &str, type_ann: &Option<Type>, initializer: Option<&Expression>, span: &Span) -> T;
 
+    // 遍历路由 完美解耦
     fn visit_stmt(&mut self, stmt: &Statement) -> T {
         match stmt {
             Statement::Block { statements, span } => self.visit_block(statements, span),
@@ -17,23 +18,28 @@ pub trait StmtVisitor<T> {
             Statement::For { init, condition, increment, body, span } => self.visit_for(init.as_deref(), condition, increment.as_deref(), body, span),
             Statement::Return { value, span } => self.visit_return(value.as_deref(), span),
             Statement::Expression { expression, span } => self.visit_expression(expression, span),
-            Statement::VariableDeclaration { name, type_ann, initializer, span } => self.visit_variable_declaration(name.as_str(), type_ann.as_deref(), initializer.as_deref(), span),
+            Statement::VariableDeclaration { name, type_ann, initializer, span } => self.visit_variable_declaration(name, type_ann, initializer.as_deref(), span),
         }
     }
 }
 
 pub trait ExprVisitor<T> {
-    fn visit_binary(&mut self, left: &Expression, operator: &str, right: &Expression, span: &Span) -> T;
-    fn visit_unary(&mut self, operator: &str, argument: &Expression, span: &Span) -> T;
-    fn visit_literal(&mut self, value: &str, span: &Span) -> T;
-    fn visit_identifier(&mut self, name: &str, span: &Span) -> T;
+    fn visit_binary(&mut self, left: &Expression, operator: &BinaryOp, right: &Expression, span: &Span) -> T;
+    fn visit_unary(&mut self, operator: &PrefixOp, argument: &Expression, span: &Span) -> T;
+    fn visit_literal(&mut self, value: &LiteralValue, span: &Span) -> T;
+    fn visit_assign(&mut self, target: &Expression, value: &Expression, span: &Span) -> T;
+    fn visit_call(&mut self, callee: &Expression, arguments: &[Expression], span: &Span) -> T;
+    fn visit_variable(&mut self, name: &str, span: &Span) -> T;
+
 
     fn visit_expr(&mut self, expr: &Expression) -> T {
         match expr {
-            Expression::Binary { left, operator, right, span } => self.visit_binary(left, operator.as_str(), right, span),
-            Expression::Unary { operator, argument, span } => self.visit_unary(operator.as_str(), argument, span),
-            Expression::Literal { value, span } => self.visit_literal(value.as_str(), span),
-            Expression::Identifier { name, span } => self.visit_identifier(name.as_str(), span),
+            Expression::Binary { left, operator, right, span } => self.visit_binary(left, operator, right, span),
+            Expression::Unary { operator, operand, span } => self.visit_unary(operator, operand, span),
+            Expression::Literal { value, span } => self.visit_literal(value, span),
+            Expression::Assign { target, value, span } => self.visit_assign(target, value, span),
+            Expression::Call { callee, arguments, span } => self.visit_call(callee, arguments, span),
+            Expression::Variable { name, span } => self.visit_variable(name, span)
         }
     }
     
